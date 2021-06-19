f5c75db3f0c321e3ffd976ec074309ffffcee8ab
Ansh <chinrust@gmail.com>
2021-06-19 22:56:35 +0530

    .

--- app/src/main/java/com/anshagrawal/dcmlkit/MainActivity.java
+++ app/src/main/java/com/anshagrawal/dcmlkit/MainActivity.java
@@ -96,6 +96,8 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
+
+
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = activityMainBinding.getRoot();
         setContentView(view);
@@ -113,13 +115,6 @@ public class MainActivity extends AppCompatActivity {
 
         activityMainBinding.empty.setBackgroundResource(R.drawable.rounded_corner);
         activityMainBinding.empty.setClipToOutline(true);
-
-//        if(activityMainBinding.myListView.getAdapter()==null){
-//            activityMainBinding.trash.setVisibility(View.GONE);
-//        }
-//        if(activityMainBinding.myListView.getAdapter()!=null){
-//            activityMainBinding.trash.setVisibility(View.VISIBLE);
-//        }
 
         activityMainBinding.myListView.setEmptyView(activityMainBinding.empty);
 
