b08462d6fcfefcce0f87531c5236503da4adfc55
Ansh <chinrust@gmail.com>
2021-06-19 21:23:21 +0530

    splash activity error fixed

--- app/src/main/java/com/anshagrawal/dcmlkit/SplashActivity.java
+++ app/src/main/java/com/anshagrawal/dcmlkit/SplashActivity.java
@@ -45,6 +45,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
             }
         }, 3000);
     }
