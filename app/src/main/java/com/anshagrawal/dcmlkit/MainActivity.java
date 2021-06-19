77c731eb6d599315b990e7183abcf5ad2fc4d18b
Ansh <chinrust@gmail.com>
2021-06-19 10:50:48 +0530

    1 pr ready app, minor changes remaining

--- app/src/main/java/com/anshagrawal/dcmlkit/MainActivity.java
+++ app/src/main/java/com/anshagrawal/dcmlkit/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.FileProvider;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.VoiceInteractor;
 import android.content.ContentResolver;
@@ -15,6 +17,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
@@ -22,10 +25,16 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.method.ScrollingMovementMethod;
+import android.transition.Transition;
+import android.transition.TransitionValues;
 import android.util.Log;
 import android.util.SparseIntArray;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -50,7 +59,10 @@ import com.otaliastudios.cameraview.BitmapCallback;
 import com.otaliastudios.cameraview.CameraListener;
 import com.otaliastudios.cameraview.FileCallback;
 import com.otaliastudios.cameraview.PictureResult;
+import com.otaliastudios.cameraview.controls.Grid;
 import com.otaliastudios.cameraview.controls.Mode;
+import com.otaliastudios.cameraview.gesture.Gesture;
+import com.otaliastudios.cameraview.gesture.GestureAction;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -69,16 +81,28 @@ public class MainActivity extends AppCompatActivity {
     int rotationAfterCrop;
     ActivityMainBinding activityMainBinding;
     String url = "https://acm-dcryptor.herokuapp.com/api/v1/";
-    String absolutePath=null;
+    String absolutePath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = activityMainBinding.getRoot();
         setContentView(view);
+        activityMainBinding.cameraView.setBackgroundResource(R.drawable.rounded_corner);
+        activityMainBinding.cameraView.setClipToOutline(true);
 
+        activityMainBinding.scannedText.setBackgroundResource(R.drawable.rounded_corner);
+        activityMainBinding.scannedText.setClipToOutline(true);
 
+        activityMainBinding.decodedText.setBackgroundResource(R.drawable.rounded_corner);
+        activityMainBinding.decodedText.setClipToOutline(true);
+
+        //makes the TextView scrollable
+        activityMainBinding.decodedText.setMovementMethod(new ScrollingMovementMethod());
+
+        activityMainBinding.cropImageView.setVisibility(View.GONE);
 
         //camera listener, listens the activity of camera
         activityMainBinding.cameraView.addCameraListener(new CameraListener() {
@@ -91,7 +115,6 @@ public class MainActivity extends AppCompatActivity {
                     public void onBitmapReady(@Nullable Bitmap bitmap) {
                         Log.i("bla", Integer.toString(result.getRotation()));
                         try {
-                            saveBitmapToCache(bitmap);
                             cropImage(bitmap);
                         } catch (IOException e) {
                             e.printStackTrace();
@@ -99,6 +122,24 @@ public class MainActivity extends AppCompatActivity {
 //                        processBitmap(bitmap, result.getRotation());
                     }
                 });
+            }
+        });
+
+
+        activityMainBinding.cameraView.mapGesture(Gesture.PINCH, GestureAction.ZOOM);
+
+        activityMainBinding.btnDecode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editedFinal = activityMainBinding.scannedText.getText().toString();
+                activityMainBinding.scannedText.setVisibility(View.GONE);
+                activityMainBinding.decodedText.setVisibility(View.VISIBLE);
+
+                try {
+                    decodeCipher(editedFinal);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -113,18 +154,12 @@ public class MainActivity extends AppCompatActivity {
                 takePhoto();
             }
         });
-
-//        activityMainBinding.btnDecode.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                activityMainBinding.scannedText.setVisibility(View.GONE);
-//                activityMainBinding.decodedText.setVisibility(View.VISIBLE);
-//            }
-//        });
     }
+
 
     //Saves the bitmap to external cache directory so that the image does not get stored in the device
     // storage and return Uri for the image cropping library
+
 
     private Uri saveBitmapToCache(Bitmap bitmap) {
         //get cache directory
@@ -132,69 +167,32 @@ public class MainActivity extends AppCompatActivity {
         cachePath.mkdirs();
         File file = new File(cachePath, "Image_123.png");
         FileOutputStream fileOutputStream;
-        try
-        {
+        try {
             fileOutputStream = new FileOutputStream(file);
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
             fileOutputStream.flush();
             fileOutputStream.close();
 
-        } catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         //get file uri
-        Uri myImageFileUri =  FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", file);
+        Uri myImageFileUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", file);
         return myImageFileUri;
     }
 
 
-    private int getImageRotation(int rotation) {
-        return rotation;
-    }
+    private void processBitmap(Bitmap bitmap, int rotation) {
+        InputImage img = InputImage.fromBitmap(bitmap, rotation);
 
-    //convert bitmap to uri which is required for crop image library
-    public Uri getImageUri(Context inContext, Bitmap inImage) throws IOException {
-        String filename = "ii";
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, filename, "blehhh");
-        Log.i("wah", path);
-        absolutePath = path;
-        bytes.close();
-
-        return Uri.parse(path);
-
-    }
-
-
-    private void processBitmap(Bitmap bitmap, int rot) {
-        InputImage img = InputImage.fromBitmap(bitmap, rot);
+        //taking an instance of google mlkit textrecognizer
         TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
         recognizer.process(img).addOnSuccessListener(new OnSuccessListener<Text>() {
             @Override
             public void onSuccess(@NonNull Text text) {
-                Log.i("bla", text.getText());
-//                Toast.makeText(MainActivity.this, text.getText(), Toast.LENGTH_SHORT).show();
                 activityMainBinding.scannedText.setText(text.getText());
-                activityMainBinding.btnDecode.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String editedOrFinal = activityMainBinding.scannedText.getText().toString();
-                        activityMainBinding.scannedText.setVisibility(View.GONE);
-                        activityMainBinding.decodedText.setVisibility(View.VISIBLE);
-
-                        try {
-                            decodeCipher(editedOrFinal);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -208,56 +206,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void takePhoto() {
         activityMainBinding.cameraView.takePictureSnapshot();
-
     }
 
     private void cropImage(Bitmap bitmap) throws IOException {
-//        Uri uri = getImageUri(this, bitmap);
-            Uri uri = saveBitmapToCache(bitmap);
-//        File file = new File(uri.getPath());
+        Uri uri = saveBitmapToCache(bitmap);
         Log.i("kl", uri.getPath());
         CropImage.activity(uri)
                 .start(this);
-
-
-    }
-
-    private void deleteImageAfterGettingUri(Uri uri) {
-//        Log.i("hg", getRealPathFromURI(MainActivity.this, absolutePath));
-//        File del= new File(getPath);
-//        Log.i("tag", del.exists()+"");
-//        Log.i("tag", del.getAbsolutePath()+"");
-//        Log.i("tag", del.getPath()+"");
-//        Log.i("tag", del.isDirectory()+"");
-//        Log.i("tag", del.isFile()+"");
-//        Log.i("tag", del.delete()+"");
-
-
-        ContentResolver contentResolver = getContentResolver();
-        contentResolver.delete(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                MediaStore.Images.ImageColumns.DATA + "=?" , new String[]{ absolutePath});
-
-
-//        if(del.exists()){
-//            Log.i("hg", "file exists, attempting to delete");
-//            if(del.delete()){
-//                Log.i("hg", "del");
-//            }
-//            else{
-//                Log.i("hg", "no del");
-//            }
-//        }
     }
 
     @Override
+    //crop image activity result listener, this block is executed when the image is cropped
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
-
-//                Log.i("vcvc", getRealPathFromURI(MainActivity.this, absolutePath));
-//                deleteImageAfterGettingUri(absolutePath);
                 Uri resultUri = result.getUri();
                 Bitmap imageAfterCrop = null;
                 try {
@@ -267,38 +231,25 @@ public class MainActivity extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                activityMainBinding.cropImageView.setVisibility(View.VISIBLE);
                 activityMainBinding.cropImageView.setImageBitmap(imageAfterCrop);
 
             }
         }
     }
-    public String getRealPathFromURI(Context context, Uri contentUri) {
-        Cursor cursor = null;
-        try {
-            String[] proj = { MediaStore.Images.Media.DATA };
-            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-    }
 
+    //processes bitmap of the cropped image
     private void ocr(Bitmap imageAfterCrop, int rotationAfterCrop) {
         processBitmap(imageAfterCrop, rotationAfterCrop);
-//        deleteImageAfterGettingUri(absolutePath);
     }
 
+    //call the api to recursively decode the cipher
     private void decodeCipher(String text) throws JSONException {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         JSONObject jsonObject = new JSONObject();
-        try{
+        try {
             jsonObject.put("data", text);
-        }
-        catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
@@ -306,12 +257,16 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(JSONObject response) {
                 try {
                     JSONArray jsonArray = response.getJSONArray("decoded_data");
-                    String finalDecodedText ="";
-                    for(int i=0;i<jsonArray.length();i++){
-                        finalDecodedText+=jsonArray.getString(i)+"\n";
+                    String finalDecodedText = "";
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        if (i < (jsonArray.length() - 1)) {
+                            finalDecodedText += jsonArray.getString(i) + "\n";
+                        } else {
+                            finalDecodedText += jsonArray.getString(i);
+                        }
                     }
 
-                    String title = jsonArray.getString(jsonArray.length()-1);
+                    String title = jsonArray.getString(jsonArray.length() - 1);
                     activityMainBinding.decodedText.setText(finalDecodedText);
 
 
@@ -327,6 +282,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         requestQueue.add(jsonObjectRequest);
-        Log.i("hg", "decodeCipher: ");
     }
 }
