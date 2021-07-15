3181611b4b57ab008b88831319ff902a983445fd
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-07-15 15:53:42 +0530

    Changes done

--- app/src/main/java/com/anshagrawal/dcmlkit/Activities/AfterSplashScreenActivity.java
+++ app/src/main/java/com/anshagrawal/dcmlkit/Activities/AfterSplashScreenActivity.java
@@ -2,22 +2,30 @@ package com.anshagrawal.dcmlkit.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TaskStackBuilder;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 
 import com.anshagrawal.dcmlkit.R;
+import com.anshagrawal.dcmlkit.UtilsService.SharedPreferencesClass;
 import com.anshagrawal.dcmlkit.databinding.ActivityAfterSplashScreenBinding;
 
 public class AfterSplashScreenActivity extends AppCompatActivity {
 
     ActivityAfterSplashScreenBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding =ActivityAfterSplashScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
 
 
         binding.signup.setOnClickListener(new View.OnClickListener() {
@@ -31,9 +39,10 @@ public class AfterSplashScreenActivity extends AppCompatActivity {
         binding.login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(AfterSplashScreenActivity.this, LoginActivity.class);
-                startActivity(i);
+                startActivity(new Intent(AfterSplashScreenActivity.this, LoginActivity.class));
             }
         });
+
+
     }
 }
--- app/src/main/java/com/anshagrawal/dcmlkit/Activities/SplashActivity.java
+++ app/src/main/java/com/anshagrawal/dcmlkit/Activities/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.anshagrawal.dcmlkit.Activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,6 +12,8 @@ import com.anshagrawal.dcmlkit.databinding.ActivitySplashBinding;
 public class SplashActivity extends AppCompatActivity {
 
     ActivitySplashBinding activitySplashBinding;
+    SharedPreferences sharedPreferencesClass;
+    SharedPreferences.Editor editor;
 
     @Override
 
@@ -20,16 +23,30 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(activitySplashBinding.getRoot());
         activitySplashBinding.logo.animate().rotation(180);
         getSupportActionBar().hide();
+        sharedPreferencesClass = this.getSharedPreferences("splash", MODE_PRIVATE);
+        editor = sharedPreferencesClass.edit();
         //launch main activity after 3.2s os splash screen
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                if (sharedPreferencesClass.getBoolean("isMain", false)) {
 
-                startActivity(new Intent(SplashActivity.this, SignUpActivity.class));
-                finish();
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+
+
+                } else {
+                    editor.putBoolean("isMain", true);
+                    editor.apply();
+
+
+                    startActivity(new Intent(SplashActivity.this, AfterSplashScreenActivity.class));
+                }
+
+
             }
 
         }, 1750);
+
         activitySplashBinding.logo.animate().rotation(180).setDuration(1200);
 
 
