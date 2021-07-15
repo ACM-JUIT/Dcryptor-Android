8e454b056266fcb117fbb441bc4147dfe37a2410
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-07-15 15:26:37 +0530

    .

--- app/src/main/java/com/anshagrawal/dcmlkit/Activities/AfterSplashScreenActivity.java
+++ app/src/main/java/com/anshagrawal/dcmlkit/Activities/AfterSplashScreenActivity.java
@@ -2,15 +2,38 @@ package com.anshagrawal.dcmlkit.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.anshagrawal.dcmlkit.R;
+import com.anshagrawal.dcmlkit.databinding.ActivityAfterSplashScreenBinding;
 
 public class AfterSplashScreenActivity extends AppCompatActivity {
+
+    ActivityAfterSplashScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_after_splash_screen);
+        binding =ActivityAfterSplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AfterSplashScreenActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AfterSplashScreenActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
